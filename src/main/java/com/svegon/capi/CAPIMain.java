bef6f762b4c6636824f6053646a7330b0ddf0c12
svegon <58487451+svegon@users.noreply.github.com>
2022-03-09 12:54:15 +0100

    Initial commit

---
+++ src/main/java/com/svegon/capi/CAPIMain.java
@@ -0,0 +1,22 @@
+package com.svegon.capi;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+public final class CAPIMain implements ClientModInitializer, DedicatedServerModInitializer, ModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+    @Override
+    public void onInitializeServer() {
+
+    }
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
